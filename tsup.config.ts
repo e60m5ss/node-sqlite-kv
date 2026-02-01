d65d738ecfc3181e86da4c5343021e3856154192
e60m5ss <notspammail@icloud.com>
2026-02-01 12:38:25 +0200

    fix(build): node protocol being removed

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     format: ["esm", "cjs"],
     entry: ["src/index.ts"],
     skipNodeModulesBundle: true,
+    removeNodeProtocol: false,
     keepNames: true,
     minify: false,
     cjsInterop: true,
